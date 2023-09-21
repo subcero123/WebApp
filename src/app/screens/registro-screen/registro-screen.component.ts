94011482c391f4c945095dc8089b9160a8ef4b28
subcero123 <subcero139@hotmail.com>
2023-09-21 14:32:55 -0600

    Servicios creados

--- src/app/screens/registro-screen/registro-screen.component.ts
+++ src/app/screens/registro-screen/registro-screen.component.ts
@@ -10,6 +10,13 @@ export class RegistroScreenComponent implements OnInit {
   public editar: boolean = false;
   public user:any = {};
 
+  public hide_1:boolean =false;
+  public hide_2:boolean =false;
+  public inputType_1:string = 'password';
+  public inputType_2:string = 'password';
+  //Deteccion de errores
+  public errors:any = {};
+
   constructor() { }
 
   ngOnInit(): void {
@@ -21,6 +28,40 @@ export class RegistroScreenComponent implements OnInit {
   public regresar(){
 
   }
+
+  public registrar(){
+
+  }
+
+  //Funciones para password  
+  showPassword()  {    
+    if(this.inputType_1 == 'password'){ 
+           this.inputType_1 = 'text';      
+           this.hide_1 = true;    
+          }    
+    else{      
+      this.inputType_1 = 'password';      
+      this.hide_1 = false;    
+    }  
+  }  
+  
+  showPwdConfirmar()  {    
+    if(this.inputType_2 == 'password'){      
+      this.inputType_2 = 'text';      
+      this.hide_2 = true;    
+    }    
+    else{      
+      this.inputType_2 = 'password';      
+      this.hide_2 = false;    
+    }  
+  }
+
+  public changeFecha(event : any){
+    console.log(event);
+    this.user.fecha_nacimiento = event.value.toISOString().split("T")[0];
+    console.log("Fecha : ", this.user.fecha_nacimiento);
+  }
+
 
   public esquemaUser(){
     return {
